40c9e3f3d11c19c82ee105a36e9288c31b8049e5
Nick Voelker <volkoff223@gmail.com>
2025-04-02 18:58:25 -0400

    Fixed bgcheck table headers

--- status-report/modules/bgreport.py
+++ status-report/modules/bgreport.py
@@ -10,8 +10,11 @@ def cleanBGData(filepath):
   # Pivit table for better orginization of data
   df = df.pivot(index='Staff Name', columns='Background Check Type', values='Expiration Date')
 
+  print(df.columns.tolist())
+
+
   # Show only these columns
-  df = df[['5-yr Background Check Submitted', '5-yr Child Abuse & Neglect', '5-yr FBI Background Check', '5-yr State Background Check']]
+  df = df[['5-yr Child Abuse & Neglect', '5-yr FBI Background Check', '5-yr State Background Check']]
 
   for col in df:
     df[col] = pd.to_datetime(df[col], errors='coerce')
